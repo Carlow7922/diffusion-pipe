233aa6df742a790dc26f711fc25e845a9c919663
Carlow7922 <78294525+Carlow7922@users.noreply.github.com>
2025-02-28 10:50:16 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /root/autodl-fs/base_models
 cd /root/autodl-fs/base_models
-git lfs clone https://hf-mirror.com/openai/clip-vit-large-patch14
+git lfs clone https://hf-mirror.com/Wan-AI/Wan2.1-T2V-14B
